bb9f52b9690e5943aa0ae1b075eb3f75058b440c
Neelay Fruitwala <neelay@lbl.gov>
2022-11-30 22:03:54 -0800

    change pulsereg

--- hdl/pulse_reg.v
+++ hdl/pulse_reg.v
@@ -1,38 +1,41 @@
 module pulse_reg #(
-    parameter PHASE_WIDTH=14,
-    parameter FREQ_WIDTH=24,
-    parameter SAMPLES_PER_CLK=4,
-    parameter TREF_WIDTH=24,
-    parameter ENV_WORD_WIDTH=24)(
+    parameter PHASE_WIDTH=17,
+    parameter FREQ_WIDTH=9,
+    parameter AMP_WIDTH=16,
+    parameter CFG_WIDTH=4, //mode + dest bits
+    parameter ENV_WORD_WIDTH=24)( //12 bit addr + 12 bit length
     input clk,
     input[PHASE_WIDTH-1:0] phase_offs_in,
     input[FREQ_WIDTH-1:0] freq_in,
-    input[TREF_WIDTH-1:0] tref,
+    input[AMP_WIDTH-1:0] amp_in, 
     input[ENV_WORD_WIDTH-1:0] env_word_in, //generally address MSB followed by length LSB
     input phase_write_en,
     input freq_write_en,
+    input amp_write_en,
     input env_word_write_en,
+    input cfg_write_en,
     input cstrobe_in,
-    output[PHASE_WIDTH-1:0] phase,
+    output reg [PHASE_WIDTH-1:0] phase,
     output reg[FREQ_WIDTH-1:0] freq,
+    output reg[AMP_WIDTH-1:0] amp,
     output reg[ENV_WORD_WIDTH-1:0] env_word,
+    output reg[CFG_WIDTH-1:0] cfg,
     output reg cstrobe);
 
-    reg[PHASE_WIDTH-1:0] phase_offs;
-    wire[FREQ_WIDTH-1:0] phase_t_acc; //phase accumulated over time
 
     always @(posedge clk) begin
         if(phase_write_en)
-            phase_offs <= phase_offs_in;
+            phase <= phase_offs_in;
         if(freq_write_en)
             freq <= freq_in;
+        if(amp_write_en)
+            amp <= amp_in;
         if(env_word_write_en)
             env_word <= env_word_in;
+        if(cfg_write_en)
+            cfg <= cfg_in;
         cstrobe <= cstrobe_in;
 
     end
     
-    assign phase_t_acc = ((freq*tref*SAMPLES_PER_CLK) >> (FREQ_WIDTH - PHASE_WIDTH));
-    assign phase = phase_t_acc[PHASE_WIDTH-1:0] + phase_offs;
-
 endmodule
