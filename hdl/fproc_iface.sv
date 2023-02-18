e1ecd0df1eefdfd75473b27bf86a7e62320dd36b
Neelay Fruitwala <neelay@lbl.gov>
2023-02-17 16:07:54 -0800

    change fproc_iface wires to logic

--- hdl/fproc_iface.sv
+++ hdl/fproc_iface.sv
@@ -1,8 +1,8 @@
 interface fproc_iface#(parameter FPROC_ID_WIDTH=8, parameter FPROC_RESULT_WIDTH=32)();
-    wire[FPROC_ID_WIDTH-1:0] id;
-    wire[FPROC_RESULT_WIDTH-1:0] data;
-    wire enable;
-    wire ready;
+    logic [FPROC_ID_WIDTH-1:0] id;
+    logic [FPROC_RESULT_WIDTH-1:0] data;
+    logic  enable;
+    logic  ready;
 
     modport proc(input data, ready, output enable, id);
     modport fproc(output data, ready, input enable, id);
