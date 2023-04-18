557f303b6e7c070c56eb2cd5e03dbc2a07a589eb
Neelay Fruitwala <neelay@lbl.gov>
2023-04-17 20:35:55 -0700

    asm prog is binary str not list

--- python/distproc/assembler.py
+++ python/distproc/assembler.py
@@ -326,7 +326,7 @@ class SingleCoreAssembler:
     def get_compiled_program(self):
         # consider splitting this into a few different functions
         # at top case level
-        cmd_list = []
+        cmd_buf = bytes()
         freq_list = []
         env_raw, env_word_map = self._get_env_buffers()
         cmd_label_addrmap = self._get_cmd_labelmap()
@@ -364,7 +364,7 @@ class SingleCoreAssembler:
                 if 'elem' in cmd.keys():
                     pulseargs['cfg_word'] = self._elem_cfgs[cmd['elem']].get_cfg_word(cmd['elem'], None)
                     
-                cmd_list.append(cg.pulse_cmd(**pulseargs))
+                cmd_buf += cg.pulse_cmd(**pulseargs).to_bytes(16, 'little')
 
             elif cmd['op'] in ['reg_alu', 'jump_cond', 'alu_fproc', 'jump_fproc', 'inc_qclk']:
                 if isinstance(cmd['in0'], str):
@@ -391,23 +391,24 @@ class SingleCoreAssembler:
                 if 'in1_reg' in cmd.keys():
                     cmd['in1_reg'] = self._regs[cmd['in1_reg']]['index']
 
-                cmd_list.append(cg.alu_cmd(cmd['op'], im_or_reg, in0, cmd.get('alu_op'), \
-                        cmd.get('in1_reg'), cmd.get('out_reg'), cmd.get('jump_addr'), cmd.get('func_id')))
+                cmd_raw = cg.alu_cmd(cmd['op'], im_or_reg, in0, cmd.get('alu_op'),
+                        cmd.get('in1_reg'), cmd.get('out_reg'), cmd.get('jump_addr'), cmd.get('func_id'))
+                cmd_buf += cmd_raw.to_bytes(16, 'little')
 
             elif cmd['op'] == 'jump_i':
                 cmd['jump_addr'] = cmd_label_addrmap[cmd['jump_label']]
-                cmd_list.append(cg.jump_i(cmd['jump_addr']))
+                cmd_buf += cg.jump_i(cmd['jump_addr']).to_bytes(16, 'little')
 
             elif cmd['op'] == 'pulse_reset':
-                cmd_list.append(cg.pulse_reset())
+                cmd_buf += cg.pulse_reset().to_bytes(16, 'little')
 
             elif cmd['op'] == 'done_stb':
-                cmd_list.append(cg.done_cmd())
+                cmd_buf += cg.done_cmd().to_bytes(16, 'little')
 
             else:
                 raise Exception('{} not supported'.format(cmd['op']))
 
-        return cmd_list, env_raw, freq_raw
+        return cmd_buf, env_raw, freq_raw
 
     def get_sim_program(self):
         """
@@ -525,7 +526,7 @@ class GlobalAssembler:
 
         for proc_group in compiled_program.proc_groups:
             elem_cfgs = {}
-            core_ind = channel_configs[proc_group[0]].core_ind
+            core_ind = str(channel_configs[proc_group[0]].core_ind)
             for chan in proc_group:
                 chan_cfg = channel_configs[chan]
                 assert chan_cfg.core_ind == core_ind
@@ -560,7 +561,7 @@ class GlobalAssembler:
         """
         assembled_prog = {}
         for core_ind, asm in self.assemblers.items():
-            cmd_list, env_raw, freq_raw = asm.get_compiled_program()
-            assembled_prog[core_ind] = {'cmd_list': cmd_list, 'env_buffers': env_raw, 'freq_buffers': freq_raw}
+            cmd_buf, env_raw, freq_raw = asm.get_compiled_program()
+            assembled_prog[core_ind] = {'cmd_list': cmd_buf, 'env_buffers': env_raw, 'freq_buffers': freq_raw}
 
         return assembled_prog
