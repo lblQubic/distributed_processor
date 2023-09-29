18c561bee4706a529fed939ff5e0b4977c088907
Neelay <you@example.com>
2023-09-29 00:33:30 -0700

    updates to qasm visitor

--- python/distproc/openqasm/visitor.py
+++ python/distproc/openqasm/visitor.py
@@ -16,6 +16,13 @@ Preliminary specification:
 
 """
 
+supported_op_dict = {
+        '==': 'eq',
+        '>': 'ge',
+        '<': 'le',
+        '+': 'add',
+        '-': 'sub'}
+
 NATIVE_INT_SIZE = 32
 
 from openqasm3.visitor import QASMVisitor, QASMNode
@@ -24,6 +31,12 @@ from distproc.openqasm.qubit_map import QubitMap, DefaultQubitMap
 from distproc.openqasm.gate_map import GateMap, DefaultGateMap
 import ipdb
 import warnings
+from attrs import define
+
+@define
+class _VariableContainer:
+    var_names: list
+    type: str = 'int'
 
 class QASMQubiCVisitor(QASMVisitor):
 
@@ -31,9 +44,11 @@ class QASMQubiCVisitor(QASMVisitor):
         self.qubit_map = qubit_map
         self.gate_map = gate_map
         self.program = []
+        self._cur_block = self.program #pointer to current instr list
 
         self.qubits = {}
         self.vars = {}
+        self._tempvar_ind = 0
         super().__init__()
 
     def visit_QubitDeclaration(self, node: QASMNode, context=None):
@@ -45,10 +60,6 @@ class QASMQubiCVisitor(QASMVisitor):
 
 
     def visit_QuantumGate(self, node: QASMNode, context=None):
-        if context is None:
-            prog = self.program
-        else:
-            prog = context
         gatename = node.name.name
         print(f"hello i am a {gatename} in {context}")
         qubits = []
@@ -62,13 +73,9 @@ class QASMQubiCVisitor(QASMVisitor):
             else:
                 raise Exception()
         
-        prog.extend(self.gate_map.get_qubic_gateinstr(gatename, qubits))
+        self._cur_block.extend(self.gate_map.get_qubic_gateinstr(gatename, qubits))
 
     def visit_QuantumReset(self, node: QASMNode, context=None):
-        if context is None:
-            prog = self.program
-        else:
-            prog = context
         qubit_reg = node.qubits.name
         if self.qubits[qubit_reg] is None:
             hardware_qubits = [self.qubit_map.get_hardware_qubit(qubit_reg, None)]
@@ -76,7 +83,7 @@ class QASMQubiCVisitor(QASMVisitor):
             hardware_qubits = [self.qubit_map.get_hardware_qubit(qubit_reg, i) for i in range(self.qubits[qubit_reg])]
 
         for qubit in hardware_qubits:
-            prog.extend([
+            self._cur_block.extend([
                 {'name': 'read', 'qubit': qubit},
                 {'name': 'branch_fproc', 'cond_lhs': 1, 'alu_cond': 'eq', 'func_id': f'{qubit}.meas', 'scope': qubit, 
                     'true': [
@@ -85,13 +92,9 @@ class QASMQubiCVisitor(QASMVisitor):
                     'false': []}])
 
     def visit_ClassicalDeclaration(self, node: QASMNode, context=None):
-        if context is None:
-            prog = self.program
-        else:
-            prog = context
         if isinstance(node.type, ast.BitType):
             if node.type.size.value is None:
-                prog.append({'name': 'declare', 'var': node.identifier.name})
+                self._cur_block.append({'name': 'declare', 'var': node.identifier.name})
                 self.vars[node.identifier.name] = [node.identifier.name]
             else:
                 self.vars[node.identifier.name] = []
@@ -99,21 +102,48 @@ class QASMQubiCVisitor(QASMVisitor):
                     warnings.warn(f'casting bit into array of {node.type.size} integers')
                     indexed_varname = f'{node.identifier.name}_{i}'
                     self.vars[node.identifier.name].append(indexed_varname)
-                    prog.append({'name': 'declare', 'var': indexed_varname})
+                    self._cur_block.append({'name': 'declare', 'var': indexed_varname})
 
         elif isinstance(node.type, ast.IntType):
             if node.type.size is not None and node.type.size != NATIVE_INT_SIZE:
                 warnings.warn(f'casting integer of size {node.type.size} to {NATIVE_INT_SIZE}')
             self.vars[node.identifier.name] = [node.identifier.name]
-            prog.append({'name': 'declare', 'var': indexed_varname})
+            self._cur_block.append({'name': 'declare', 'var': indexed_varname})
 
     def visit_BranchingStatement(self, node: QASMNode, context=None):
-        if context is None:
-            prog = self.program
-        else:
-            prog = context
         print(f'hello i am a branch in {context}')
 
-        cond_lhs = self.vars[node.condition.lhs.argument.name]
+        expr = self.visit(node.condition) #parse out the conditional expression
 
-        prog.append({'name': 'branch_var', 'cond_lhs': 
+
+        #self._cur_block.append({'name': 'branch_var', 'cond_lhs': })
+
+    def visit_BinaryExpression(self, node: QASMNode, context=None):
+        """
+        context has two modes:
+            None: default; return a tuple (lhs, op, rhs) in terms of strings/primitive var names
+            'primitive': return a single token containing the result of the expression, and 
+                        add program statements declaring, setting this token accordingly
+        """
+        lhs_primitive = self.visit(node.lhs, 'primitive')
+        rhs_primitive = self.visit(node.rhs, 'primitive')
+        op = node.op.name
+
+        if context == 'primitive':
+            if lhs_primitive[:10] == '_temp_var_':
+                self._cur_block.append({'name': 'alu', 'op': supported_op_dict[op], 'lhs': lhs_primitive, 
+                                        'rhs': rhs_primitive, 'out': lhs_primitive})
+                return lhs_primitive
+            elif rhs_primitive[:10] == '_temp_var_':
+                self._cur_block.append({'name': 'alu', 'op': supported_op_dict[op], 'lhs': lhs_primitive, 
+                                        'rhs': rhs_primitive, 'out': rhs_primitive})
+                return rhs_primitive
+            else:
+                temp_out = f'_temp_var_{self._tempvar_ind}'
+                self._cur_block.append({'name': 'declare', 'var': temp_out})
+                self._tempvar_ind += 1
+                self._cur_block.append({'name': 'alu', 'op': supported_op_dict[op], 'lhs': lhs_primitive, 
+                                        'rhs': rhs_primitive, 'out': temp_out})
+                return temp_out
+
+
